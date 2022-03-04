d11ccfd6f6ab6d1c406c1c2cf0ac3a8e34c68f4c
bansheep <37919167+bansheep@users.noreply.github.com>
2022-03-04 11:25:37 -0700

    Update TimeAndTimeAgain.java

--- TimeAndTimeAgain/src/TimeAndTimeAgain.java
+++ TimeAndTimeAgain/src/TimeAndTimeAgain.java
@@ -5,39 +5,16 @@ import java.util.regex.Pattern;
 public class TimeAndTimeAgain {
 
 	public static void main(String[] args) {
-		try (Scanner input = new Scanner(System.in)) {
-			int testCases = Integer.parseInt(input.nextLine());
-
-			Pattern secondRegex = Pattern.compile(".*?(\\d+)s.*");
-			Pattern minuteRegex = Pattern.compile(".*?(\\d+)m.*");
-			Pattern hourRegex = Pattern.compile(".*?(\\d+)h.*");
-
-			for (int testcase = 0; testcase < testCases; testcase++) {
-				String data = input.nextLine();
-
-				int sec = 0;
-				int min = 0;
-				int hour = 0;
-
-				Matcher matcher = secondRegex.matcher(data);
-				if (matcher.matches()) {
-					sec = Integer.parseInt(matcher.group(1));
-				}
-				matcher = minuteRegex.matcher(data);
-				if (matcher.matches()) {
-					min = Integer.parseInt(matcher.group(1));
-				}
-				matcher = hourRegex.matcher(data);
-				if (matcher.matches()) {
-					hour = Integer.parseInt(matcher.group(1));
-				}
-
-				String output = String.format("%02d:%02d:%02d", Integer.valueOf(hour), Integer.valueOf(min),
-						Integer.valueOf(sec));
-				
-				System.out.println(output);
-			}
-		}
+		System.out.println("Hello World");
+		
+		// Format needed HH:MM:SS
+		// Read in input
+		// First number is the number of items
+		
+		// Loop through content
+		
+		
+		// Print out the numbers in appropriate format
 	}
 
 }
